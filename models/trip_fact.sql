e6267b22081480d21af504d311738d50e1722f8d
romm.kirichenko@gmail.com <romm.kirichenko@gmail.com>
2025-09-07 22:37:39 +0000

    trip_fact modle added

---
+++ models/trip_fact.sql
@@ -0,0 +1,14 @@
+with TRIPS as (
+    select 
+        RIDE_ID,
+        RIDEABLE_TYPE,
+        DATE(TO_TIMESTAMP(STARTED_AT)) AS TRIP_DATE,
+        START_STATION_ID,
+        END_STATION_ID,
+        MEMBER_CASUAL,
+        TIMESTAMPDIFF(SECOND, TO_TIMESTAMP(STARTED_AT), TO_TIMESTAMP(ENDED_AT)) AS TRIP_DURATION_SECONDS
+    from {{ source('demo', 'bike') }}
+    where RIDE_ID != 'ride_id'
+)
+
+select * from TRIPS 
